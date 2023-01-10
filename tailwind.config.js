14bcef9c5c44b66e3e2ca456d944f10a8c0912d3
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2023-01-10 15:17:20 +0500

    added icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	content: ['./**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+	content: ['./*.html', './src/js/**/*.js'],
 	darkMode: 'class',
 	theme: {
 		container: {
