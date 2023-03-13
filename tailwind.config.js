3043bcc5c09685ac6f15239531bfc1e42d8380a9
Shift <shift@laravelshift.com>
2023-03-13 10:03:53 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,36 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	content: ['./index.html', './src/js/**/*.js'],
-	darkMode: 'class',
-	theme: {
-		container: {
-			center: true,
-			padding: '1.25rem',
-		},
-		screens: {
-			'sm': '587px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1180px',
-		},
-		colors: {
-			blue: {
-				300: '#C7E8FF',
-				500: '#247CB8',
-			},
-			gray: {
-				100: '#F7F7F8',
-				300: '#68676B',
-			},
-			white: colors.white,
-			black: colors.black,
-		},
-		extend: {
-			fontFamily: {
-				sans: ['i-roboto', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/line-clamp'),
-	],
+  content: ['./index.html', './src/js/**/*.js'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '1.25rem',
+    },
+    screens: {
+      sm: '587px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1180px',
+    },
+    colors: {
+      blue: {
+        300: '#C7E8FF',
+        500: '#247CB8',
+      },
+      gray: {
+        100: '#F7F7F8',
+        300: '#68676B',
+      },
+      white: colors.white,
+      black: colors.black,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['i-roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
 }
